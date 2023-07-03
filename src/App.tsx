13f6cdaf4b178738627fa09eab8a65d257fdd05e
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2023-07-02 19:59:53 -0700

    create basic template for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+         setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // TODO: perform calculation here with mathjs
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                value="inputValue"
+                onChange={handleInputChange}
+                placeholder="enter an algebraic expression"
+                />
+                <button type="submit">Calculate</button>
+            </form>
+        </div>
     </div>
-  );
-}
 
+  );
+
+};
 export default App;
